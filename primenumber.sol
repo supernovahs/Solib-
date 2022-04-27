a55a264d521170ce37067642bf9dc833fd640377
supernovahs <harshitsinghal252@gmail.com>
2022-04-27 19:10:22 +0530

    Prime

---
+++ primenumber.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.8.0;
+
+pragma solidity ^0.8.13;
+library Prime{
+    function isprime(uint num) public view returns (bool){
+        bool result;
+        assembly{
+        
+            if eq(num,2){
+                result := true;
+            }
+
+            else if eq(num %2,0){
+                result:=true;
+            }
+
+            else{
+                result:= true;
+                for(uint i = 3; i < num; i += 2){
+                    switch eq(num % i,0){
+                        case true{
+                            result:= false;
+                            break;
+                        }
+                        {
+                            result:= true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
