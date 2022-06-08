daef340191df5236b95cc4a3027b97ea3ee171dd
supernovahs <harshitsinghal252@gmail.com>
2022-06-08 16:02:43 +0530

    Square root init

---
+++ squareroot.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.13;
+
+// @notice Library written in assembly to calculate the nearest square root of a number
+// @author Supernovahs.eth <harshitsinghal252@gmail.com>
+library SQRT{
+
+    // Currently in development stage 
+    // Feel free to contribute to this library in the issues section of the github repo
+}
